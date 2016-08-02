b9893ee7f7269e4a95ee00383c62df7fe405e8d6
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-08-01 22:23:47 -0300

    Small bugs fixed on battery script.

--- battery.py
+++ battery.py
@@ -3,11 +3,15 @@ import re
 
 command = sbp.check_output('acpi')
 command = command.split()
-value = command[3]
-state = command[2]
 
-value = re.search(r'\d\d', str(value)).group()
-state = re.search(r'[CD]', str(state)).group()
+value = re.search(r'\d\d', str(command[3])).group()
+state = re.search(r'[CD]', str(command[2])).group()
+
+if(int(value) == 10):
+    try:
+        value = re.search(r'\d\d\d', str(command[3])).group()
+    except:
+        pass
 
 if(state == 'C'):
     state = "Charg."
